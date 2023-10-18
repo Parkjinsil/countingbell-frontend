e411250d2705281a83521b2f173db66298c0f8a6
rladbtjd10 <133620500+rladbtjd10@users.noreply.github.com>
2023-10-19 00:46:32 +0900

    feat:2023.10.19 discount 수정 - 경미

--- src/pages/Discount.js
+++ src/pages/Discount.js
@@ -4,7 +4,13 @@ import { Container } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import styled from "styled-components";
 import { useDispatch } from "react-redux";
-import { asyncAddDiscount } from "../store/discountSlice";
+import {
+  asyncAddDiscount,
+  //asyncViewDiscounts,
+  asyncViewDiscount,
+  asyncUpdateDiscount,
+  asyncDeleteDiscount,
+} from "../store/discountSlice";
 
 const H1 = styled.h1`
   font-size: 20px;
@@ -14,47 +20,117 @@ const H1 = styled.h1`
 `;
 
 const Discount = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const onAddDiscount = (e) => {
+    e.preventDefault(); //폼 제출을 중지하여 페이지가 다시 로드되지 않게
 
-    const onAddDiscount = (e) => {
-        
-        e.preventDefault(); 
+    const formData = new FormData();
+    formData.append("disDesc", e.target.disDesc.value);
+    formData.append("disPeriod", e.target.disPeriod.value);
+    formData.append("resCode", e.target.resCode.value);
 
-        const formData = new FormData(); 
-        formData.append("disDesc", e.target.disDesc.value);
-        formData.append("disPeriod", e.target.disPeriod.value);
-        formData.append("resCode", e.target.resCode.value);
+    console.log(formData);
 
-        console.log(formData);
-    
-        dispatch(asyncAddDiscount(formData));
-      };
+    dispatch(asyncAddDiscount(formData));
+  };
 
-      return (
-        <div className="container my-5">
-      <div className="position-relative p-5 text-center text-muted bg-body border border-dashed rounded-5" style={{marginTop:'100px'}}>
+  const onSearchDiscount = (e) => {
+    e.preventDefault();
+    // 할인 조회 기능을 구현합니다.
+    // 예를 들어, 입력된 식당 코드 (e.target.resCode.value)를 사용하여 할인 정보를 조회할 수 있습니다.
+    dispatch(asyncViewDiscount(e.target.resCode.value));
+  };
+
+  const onUpdateDiscount = (e) => {
+    e.preventDefault();
+    // 할인 수정 기능을 구현합니다.
+    // 예를 들어, 입력된 할인 설명, 기간 등을 사용하여 해당 할인을 업데이트할 수 있습니다.
+    dispatch(
+      asyncUpdateDiscount({
+        disDesc: e.target.disDesc.value,
+        disPeriod: e.target.disPeriod.value,
+        resCode: e.target.resCode.value,
+      })
+    );
+  };
+
+  const onCancelDiscount = (e) => {
+    e.preventDefault();
+    // 할인 취소 기능을 구현합니다.
+    // 예를 들어, 입력된 식당 코드 (e.target.resCode.value)를 사용하여 해당 식당의 할인을 취소할 수 있습니다.
+    dispatch(asyncDeleteDiscount(e.target.resCode.value));
+  };
+
+  return (
+    <div className="container my-5">
+      <div
+        className="position-relative p-5 text-center text-muted bg-body border border-dashed rounded-5"
+        style={{ marginTop: "100px" }}
+      >
         <Container>
           <H1>할인</H1>
-          <Form onSubmit={onAddDiscount} style={{ width: "600px", margin: "0 auto" }}>
+          <Form
+            style={{ width: "600px", margin: "0 auto" }}
+            onSubmit={onSearchDiscount}
+          >
             <Form.Group className="mb-3">
               <Form.Control type="text" placeholder="할인설명" name="disDesc" />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control type="text" placeholder="할인기간" name="disPeriod" />
+              <Form.Control
+                type="text"
+                placeholder="할인기간"
+                name="disPeriod"
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Control type="text" placeholder="식당코드" name="resCode" />
             </Form.Group>
-            <Form.Group className="mb-3">
-              <button type="submit" className="btn btn-primary">할인등록</button>
+            <Form.Group className="mb-3" style={{ width: "700px" }}>
+              <span className="btn1" style={{ paddingRight: "25px" }}>
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  onClick={onAddDiscount}
+                >
+                  할인등록
+                </button>
+              </span>
+              <span className="btn2" style={{ paddingRight: "25px" }}>
+                <button type="submit" className="btn btn-primary">
+                  할인전체조회
+                </button>
+              </span>
+              <span className="btn3" style={{ paddingRight: "25px" }}>
+                <button type="submit" className="btn btn-primary">
+                  할인조회
+                </button>
+              </span>
+              <span className="btn4" style={{ paddingRight: "25px" }}>
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  onClick={onUpdateDiscount}
+                >
+                  할인등록수정
+                </button>
+              </span>
+              <span className="btn5">
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  onClick={onCancelDiscount}
+                >
+                  할인등록취소
+                </button>
+              </span>
             </Form.Group>
-            
           </Form>
         </Container>
-        </div>
-        </div>
-      );
-    };
-    
-    export default Discount;
+      </div>
+    </div>
+  );
+};
+
+export default Discount;
--- src/store/discountSlice.js
+++ src/store/discountSlice.js
@@ -1,45 +1,66 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { addDiscount } from "../api/Discount";
+import {
+  addDiscount,
+  getDiscounts,
+  getDiscount,
+  postDiscount,
+  delDiscount,
+} from "../api/Discount";
 
 // 할인 추가 비동기 액션 생성
-const asyncAddDiscount = createAsyncThunk("discountSlice/asyncAddDiscount", async (data) => {
-  try {
-    const result = await addDiscount(data);
-    return result.data;
-  } catch (error) {
-    throw error;
+const asyncAddDiscount = createAsyncThunk(
+  "discountSlice/asyncAddDiscount",
+  async (data) => {
+    try {
+      const result = await addDiscount(data);
+      return result.data;
+    } catch (error) {
+      throw error;
+    }
   }
-});
+);
 
-// 할인 조회
-const asyncViewDiscount = createAsyncThunk(
-  "discountSilce/asyncViewDiscount", async (page) => {
-    const result = await asyncViewDiscount(page);
+// 할인 전체 조회
+const asyncViewDiscounts = createAsyncThunk(
+  "discountSlice/asyncViewDiscounts",
+  async (page) => {
+    const result = await getDiscounts(page);
     return result.data;
-  });
+  }
+);
+
+// 식당1개에 따른 할인 조회
+const asyncViewDiscount = createAsyncThunk(
+  "discountSlice/asyncViewDiscount",
+  async (id) => {
+    const result = await getDiscount(id);
+    return result.data;
+  }
+);
 
 // 할인 수정
 const asyncUpdateDiscount = createAsyncThunk(
-  "discountSile/asyncUpdateDiscount", async (data) => {
-    const result = await asyncUpdateDiscount(data);
+  "discountSlice/asyncUpdateDiscount",
+  async (data) => {
+    const result = await postDiscount(data);
     return result.data;
-  });
+  }
+);
 
 // 할인 삭제
 const asyncDeleteDiscount = createAsyncThunk(
-  "discountSlice/asyncDeleteDiscount", async (id) => {
-    const result = await asyncDeleteDiscount(id);
+  "discountSlice/asyncDeleteDiscount",
+  async (id) => {
+    const result = await delDiscount(id);
     return result.data;
   }
-)
-
+);
 
 const discountSlice = createSlice({
   name: "discountSlice",
   initialState: { data: null, error: null, loading: false },
   reducers: {},
   extraReducers: (builder) => {
-
     // 할인 추가 액션 성공,실패,로딩시 상태 업데이트
     builder
       //실패
@@ -48,64 +69,78 @@ const discountSlice = createSlice({
         state.loading = false;
       })
       // 액션이 성공한 경우- 데이터 저장
-      .addCase(asyncAddDiscount.fulfilled, (state, action) => { 
+      .addCase(asyncAddDiscount.fulfilled, (state, action) => {
         state.data = action.payload;
         state.loading = false;
       })
-      // 액션이 아직 처리중일 때의 상태 
+      // 액션이 아직 처리중일 때의 상태
       .addCase(asyncAddDiscount.pending, (state) => {
         state.loading = true;
       });
 
+    // 할인 전체 조회 액션 성공,실패,로딩시 상태 업데이트
+    builder
+      .addCase(asyncViewDiscounts.rejected, (state, action) => {
+        state.error = "할인 조회에 실패했습니다. 다시 시도해주세요.";
+        state.loading = false;
+      })
+      .addCase(asyncViewDiscounts.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.loading = false;
+      })
+      .addCase(asyncViewDiscounts.pending, (state) => {
+        state.loading = true;
+      });
 
-      // 할인 조회 액션 성공,실패,로딩시 상태 업데이트
-      builder
+    // 할인 조회 액션 성공,실패,로딩시 상태 업데이트
+    builder
       .addCase(asyncViewDiscount.rejected, (state, action) => {
         state.error = "할인 조회에 실패했습니다. 다시 시도해주세요.";
         state.loading = false;
       })
       .addCase(asyncViewDiscount.fulfilled, (state, action) => {
-        state.data = action.payload; 
+        state.data = action.payload;
         state.loading = false;
       })
-      .addCase(asyncViewDiscount.pending, (state) => { 
+      .addCase(asyncViewDiscount.pending, (state) => {
         state.loading = true;
       });
 
-      // 할인 수정 액션 성공,실패,로딩시 상태 업데이트
-      builder
+    // 할인 수정 액션 성공,실패,로딩시 상태 업데이트
+    builder
       .addCase(asyncUpdateDiscount.rejected, (state, action) => {
         state.error = "할인 수정에 실패했습니다. 다시 시도해주세요.";
         state.loading = false;
       })
       .addCase(asyncUpdateDiscount.fulfilled, (state, action) => {
-        state.data = action.payload; 
+        state.data = action.payload;
         state.loading = false;
       })
-      .addCase(asyncUpdateDiscount.pending, (state) => { 
+      .addCase(asyncUpdateDiscount.pending, (state) => {
         state.loading = true;
       });
 
-      // 할인 삭제 액션 성공,실패,로딩시 상태 업데이트
-      builder
+    // 할인 삭제 액션 성공,실패,로딩시 상태 업데이트
+    builder
       .addCase(asyncDeleteDiscount.rejected, (state, action) => {
         state.error = "할인 수정에 실패했습니다. 다시 시도해주세요.";
         state.loading = false;
       })
       .addCase(asyncDeleteDiscount.fulfilled, (state, action) => {
-        state.data = action.payload; 
+        state.data = action.payload;
         state.loading = false;
       })
-      .addCase(asyncDeleteDiscount.pending, (state) => { 
+      .addCase(asyncDeleteDiscount.pending, (state) => {
         state.loading = true;
       });
-    
-
-
-
   },
 });
 
-
 export default discountSlice;
-export { asyncAddDiscount };
+export {
+  asyncAddDiscount,
+  asyncViewDiscounts,
+  asyncViewDiscount,
+  asyncUpdateDiscount,
+  asyncDeleteDiscount,
+};
